8e9ecf357bf231fdf062e61b03b03194e345963a
nickyoung-github <nick.young.github@gmail.com>
2025-01-06 11:22:17 -0500

    Fix version and package structure

--- pydantic_gubbins/__init__.py
+++ pydantic_gubbins/__init__.py
@@ -1,14 +1,9 @@
 from .main import BaseModel, ModelMetaclass
-from ._typing import DiscriminatedUnion, FrozenDict, SubclassOf, Union
 
 # ToDo: add one of the things that handles this automagically
-__version__ = "1.0.1"
+__version__ = "1.0.0"
 
 __all__ = (
     BaseModel,
-    DiscriminatedUnion,
-    FrozenDict,
-    ModelMetaclass,
-    SubclassOf,
-    Union
+    ModelMetaclass
 )
