3508b6f21a9dca9b6c1ea1bf6ab445036de0104f
nickyoung-github <nick.young.github@gmail.com>
2025-01-06 10:31:26 -0500

    Bump version

--- pydantic_gubbins/__init__.py
+++ pydantic_gubbins/__init__.py
@@ -2,7 +2,7 @@ from .main import BaseModel, ModelMetaclass
 from ._typing import DiscriminatedUnion, FrozenDict, SubclassOf, Union
 
 # ToDo: add one of the things that handles this automagically
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = (
     BaseModel,
