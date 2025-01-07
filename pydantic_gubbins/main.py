86966991b9c75f241cec78f7d4394a1c54111150
nickyoung-github <nick.young.github@gmail.com>
2025-01-07 12:48:31 -0500

    Code format OCD

--- pydantic_gubbins/main.py
+++ pydantic_gubbins/main.py
@@ -62,18 +62,14 @@ class BaseModel(_BaseModel, metaclass=ModelMetaclass):
         return super().__eq__(other) and\
             all(v1 == v2 for (_, v1), (_, v2) in zip(self.__descriptor_items(), other.__descriptor_items()))
 
+    def __repr_args__(self):
+        yield from (kv for kv in chain(self.__descriptor_items(), super().__repr_args__()))
+
     def __set_descriptor_values__(self, values: dict[str, Any]):
         # This will be called with all descriptor values when __dict__ is assigned.
         # Override if you want custom behaviour, such as constructing a storage object
         for key, value in values.items():
             self.__set_descriptor_value(key, value)
-
-    @model_serializer(mode='wrap')
-    def include_descriptors(self, handler: SerializerFunctionWrapHandler) -> Any:
-        return {**handler(self), **dict(self.__descriptor_items())}
-
-    def __repr_args__(self):
-        yield from (kv for kv in chain(self.__descriptor_items(), super().__repr_args__()))
 
     def __descriptor_items(self):
         yield from ((fld, getattr(self, fld)) for fld in self.__pydantic_descriptor_fields__)
@@ -82,3 +78,7 @@ class BaseModel(_BaseModel, metaclass=ModelMetaclass):
         if self.model_config.get('validate_assignment', False):
             self.__pydantic_validator__.validate_assignment(self.model_construct(), key, value)
         object.__setattr__(self, key, value)
+
+    @model_serializer(mode='wrap')
+    def include_descriptors(self, handler: SerializerFunctionWrapHandler) -> Any:
+        return {**handler(self), **dict(self.__descriptor_items())}
